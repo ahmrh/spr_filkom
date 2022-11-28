01d32eac82ac9a08182207953d4afb389631634f
reza <79359426+ahmrh@users.noreply.github.com>
2022-11-28 09:55:17 +0700

    Add files via upload
    adding project structure and daftar ruangan view

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# Sistem Peminjaman Ruangan FILKOM UB
-
-## Dalam rangka menyelesaikan proyek akhir mata kuliah Rekayasa Perangkat Lunak
-
-### RPL - C Kelompok 6
-
-
+# spr_filkom
+# spr_filkom
---
+++ views/DaftarRuangan/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/style.css">
+    <title>Daftar Ruangan</title>
+</head>
+<body>
+
+    <div class="row">
+
+    <aside>
+        <nav>
+            <ul>
+                <li>
+                    <a href="./">
+                        <img src="../assets/images/home-icon.png" alt="" srcset="">
+                        <p>Beranda</p>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="./">
+                        <img src="../assets/images/ruang-icon.png" width=40 alt="" srcset="">
+                        <p>Daftar Ruangan</p>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="./">
+                        <img src="../assets/images/peminjaman-icon.png" width=40 alt="" srcset="">
+                        <p>Daftar Peminjaman</p>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="./">
+                        <img src="../assets/images/contact-icon.png" width=40 alt="" srcset="">
+                        <p>Hubungi Kami</p>
+                    </a>
+                </li>
+
+
+            </ul>
+        </nav>
+        <button id="keluar" class="light">Keluar</button>
+    </aside>
+    <main>
+        <h1>Alur Peminjaman Ruangan <br> FILKOM UB</h1>
+        <ol>
+            <li>Memilih ruangan yang dibutuhkan dan pastikan ruangan tersebut tersedia</li>
+            <li>Mengisi form peminjaman yang disediakan oleh sistem </li>
+            <li>Menunggu konfirmasi dari pihak admin</li>
+            <li>Mengirimkan KTM dan Dokumen yang diperlukan ke pihak admin di <b>ruang A 1.1</b></li>
+            <li>Setelah ruangan selesai, kembali ke pihak admin untuk mengambil KTM dan menandakan ruangan telah selesai digunakan</li>
+        </ol>
+        <button>Pinjam Ruangan</button>
+
+    </main>
+
+    </div>
+</body>
+</html>
